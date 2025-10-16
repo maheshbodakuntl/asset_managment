0b906f951fb1947c668168e5a6753702b915c338
maheshbodakuntl <maheshbodakuntla108@gmail.com>
2025-10-16 16:23:02 +0530

    db change

--- Assests_Be/config/db.js
+++ Assests_Be/config/db.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  port: process.env.DB_PORT,
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for Render SSL
+    },
+  },
   logging: false,
 });
 
